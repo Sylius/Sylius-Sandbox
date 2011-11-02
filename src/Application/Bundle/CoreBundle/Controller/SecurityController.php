80a4a8981a597b34d377c06f4df434ec7b37d5a1
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2011-11-02 20:21:45 +0100

    some fixes with sorting and security.

--- src/Application/Bundle/CoreBundle/Controller/SecurityController.php
+++ src/Application/Bundle/CoreBundle/Controller/SecurityController.php
@@ -11,7 +11,9 @@
 
 namespace Application\Bundle\CoreBundle\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
@@ -24,6 +26,12 @@ class SecurityController extends ContainerAware
 {
     public function loginAction()
     {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        
+        if (is_object($user) && $user instanceof UserInterface) {
+            return new RedirectResponse($this->container->get('router')->generate('app_core_frontend'));
+        }
+        
         $request = $this->container->get('request');
         $session = $request->getSession();
 
