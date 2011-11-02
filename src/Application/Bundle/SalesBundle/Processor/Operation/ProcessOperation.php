8e6f0243aa4298de1bba5419f9a9f774fe52ffe1
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2011-11-02 19:23:51 +0100

    order preparation added.

--- src/Application/Bundle/SalesBundle/Processor/Operation/ProcessOperation.php
+++ src/Application/Bundle/SalesBundle/Processor/Operation/ProcessOperation.php
@@ -8,15 +8,18 @@ use Sylius\Bundle\SalesBundle\Processor\Operation\OperationInterface;
 
 class ProcessOperation extends ContainerAware implements OperationInterface
 {
-    public function process(OrderInterface $order)
-    {      
-        // Lock cart.
+    public function prepare(OrderInterface $order)
+    {
         $cart = $this->container->get('sylius_cart.provider')->getCart();
         
         if ($cart->isEmpty()) {
             throw new \LogicException('The cart must be not empty.');
         }
-        
+    }
+    
+    public function process(OrderInterface $order)
+    {      
+        $cart = $this->container->get('sylius_cart.provider')->getCart();
         $cart->setLocked(true);
         
         // Set order value.
