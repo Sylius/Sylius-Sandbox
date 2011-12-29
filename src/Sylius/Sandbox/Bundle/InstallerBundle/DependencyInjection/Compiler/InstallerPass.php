379b9d60883f3c65cf8e0b9878953ad045e73f26
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2011-12-29 13:11:31 +0100

    disable theming listener for installer.

---
+++ src/Sylius/Sandbox/Bundle/InstallerBundle/DependencyInjection/Compiler/InstallerPass.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Sandbox\Bundle\InstallerBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * Compiler pass that registers all setups.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class InstallerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->getParameter('sylius.installed')) {
+            $container->removeDefinition('sylius_theming.listener.request');
+        }
+    }
+}
--- src/Sylius/Sandbox/Bundle/InstallerBundle/SandboxInstallerBundle.php
+++ src/Sylius/Sandbox/Bundle/InstallerBundle/SandboxInstallerBundle.php
@@ -2,8 +2,14 @@
 
 namespace Sylius\Sandbox\Bundle\InstallerBundle;
 
+use Sylius\Sandbox\Bundle\InstallerBundle\DependencyInjection\Compiler\InstallerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SandboxInstallerBundle extends Bundle
-{ 
+{
+    public function build(ContainerBuilder $builder)
+    {
+        $builder->addCompilerPass(new InstallerPass());
+    }
 }
