2130b15b40011368c96851c3da114a1938f94c9d
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-03-27 23:29:41 +0200

    two ways to add item, by POST request with form, and by GET with default quantity 1.

--- src/Sylius/Sandbox/Bundle/CartBundle/Resolver/ItemResolver.php
+++ src/Sylius/Sandbox/Bundle/CartBundle/Resolver/ItemResolver.php
@@ -14,6 +14,7 @@ namespace Sylius\Sandbox\Bundle\CartBundle\Resolver;
 use Sylius\Bundle\AssortmentBundle\Model\ProductManagerInterface;
 use Sylius\Bundle\CartBundle\Model\ItemManagerInterface;
 use Sylius\Bundle\CartBundle\Resolver\ItemResolverInterface;
+use Symfony\Component\Form\FormFactory;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -39,15 +40,23 @@ class ItemResolver implements ItemResolverInterface
     private $productManager;
 
     /**
+     * Form factory.
+     *
+     * @var FormFactory
+     */
+    private $formFactory;
+
+    /**
      * Constructor.
      *
      * @param ItemManagerInterface    $itemManager
      * @param ProductManagerInterface $productManager
      */
-    public function __construct(ItemManagerInterface $itemManager, ProductManagerInterface $productManager)
+    public function __construct(ItemManagerInterface $itemManager, ProductManagerInterface $productManager, FormFactory $formFactory)
     {
         $this->itemManager = $itemManager;
         $this->productManager = $productManager;
+        $this->formFactory = $formFactory;
     }
 
     /**
@@ -55,10 +64,29 @@ class ItemResolver implements ItemResolverInterface
      */
     public function resolveItemToAdd(Request $request)
     {
+        /*
+         * We're getting here product id via query but you can easily override route
+         * pattern and use attributes, which are available through request object.
+         */
         if ($id = $request->query->get('id')) {
             if ($product = $this->productManager->findProduct($id)) {
+                /*
+                 * To have it flexible, we allow adding single item by GET request
+                 * and also user can provide desired quantity by form via POST request.
+                 */
                 $item = $this->itemManager->createItem();
                 $item->setProduct($product);
+
+                if ('POST' === $request->getMethod()) {
+                    $form = $this->formFactory->create('sylius_cart_item');
+                    $form->setData($item);
+                    $form->bindRequest($request);
+
+                    if (!$form->isValid()) {
+
+                        return false;
+                    }
+                }
 
                 return $item;
             }
