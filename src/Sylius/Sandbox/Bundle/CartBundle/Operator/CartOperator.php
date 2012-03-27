8303bfde2573500bcd8005ce5e7f89e8ff8cbf90
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-03-28 00:12:17 +0200

    use default operator as base one.

--- src/Sylius/Sandbox/Bundle/CartBundle/Operator/CartOperator.php
+++ src/Sylius/Sandbox/Bundle/CartBundle/Operator/CartOperator.php
@@ -12,62 +12,25 @@
 namespace Sylius\Sandbox\Bundle\CartBundle\Operator;
 
 use Sylius\Bundle\CartBundle\Model\CartInterface;
-use Sylius\Bundle\CartBundle\Model\ItemInterface;
-use Sylius\Bundle\CartBundle\Operator\CartOperatorInterface;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Sylius\Bundle\CartBundle\Operator\CartOperator as BaseCartOperator;
 
 /**
  * Cart operator.
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
-class CartOperator extends ContainerAware implements CartOperatorInterface
+class CartOperator extends BaseCartOperator
 {
-    public function addItem(CartInterface $cart, ItemInterface $item)
-    {
-        $product = $item->getProduct();
-
-        foreach ($cart->getItems() as $existingItem) {
-            if ($existingItem->getProduct() === $product) {
-                $existingItem->setQuantity($existingItem->getQuantity() + $item->getQuantity());
-
-                return;
-            }
-        }
-
-        $cart->addItem($item);
-    }
-
-    public function removeItem(CartInterface $cart, ItemInterface $item)
-    {
-        $cart->removeItem($item);
-    }
-
     public function refresh(CartInterface $cart)
     {
         $value = 0.00;
-        $totalItems = $cart->countItems();
 
         foreach ($cart->getItems() as $item) {
             $value += $item->getProduct()->getPrice() * $item->getQuantity();
         }
 
         $cart->setValue($value);
-        $cart->setTotalItems($totalItems);
-    }
 
-    public function validate(CartInterface $cart)
-    {
-        return true;
-    }
-
-    public function clear(CartInterface $cart)
-    {
-        $this->container->get('sylius_cart.manager.cart')->removeCart($cart);
-    }
-
-    public function save(CartInterface $cart)
-    {
-        $this->container->get('sylius_cart.manager.cart')->persistCart($cart);
+        parent::refresh($cart);
     }
 }
