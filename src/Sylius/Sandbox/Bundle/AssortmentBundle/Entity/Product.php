557a9a27e5f3491daf0e519e383657145ee59506
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-04-14 00:40:08 +0200

    add proxy method to product model, in order to access master variant.

--- src/Sylius/Sandbox/Bundle/AssortmentBundle/Entity/Product.php
+++ src/Sylius/Sandbox/Bundle/AssortmentBundle/Entity/Product.php
@@ -16,15 +16,42 @@ use Sylius\Bundle\AssortmentBundle\Entity\CustomizableProduct as BaseProduct;
 
 class Product extends BaseProduct
 {
+    /**
+     * Product category.
+     *
+     * @var CategoryInterface
+     */
     protected $category;
 
+    /**
+     * Get category.
+     *
+     * @return CategoryInterface
+     */
     public function getCategory()
     {
         return $this->category;
     }
 
+    /**
+     * Set category.
+     *
+     * @param CategoryInterface $category
+     */
     public function setCategory(CategoryInterface $category)
     {
         $this->category = $category;
+    }
+
+    /**
+     * This is a proxy method to access master variant price.
+     * Because if there are no options/variants defined, the master variant is
+     * the project.
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->masterVariant->getPrice();
     }
 }
