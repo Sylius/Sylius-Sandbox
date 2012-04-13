249158f93843ca20fc9da4421e40d998babf9f39
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-04-13 22:11:50 +0200

    added variant form type and finished create product template.

---
+++ src/Sylius/Sandbox/Bundle/AssortmentBundle/Form/Type/VariantType.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Sylius sandbox application.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Sandbox\Bundle\AssortmentBundle\Form\Type;
+
+use Sylius\Bundle\AssortmentBundle\Form\Type\VariantType as BaseVariantType;
+use Symfony\Component\Form\FormBuilder;
+
+/**
+ * Product variant type. We need to add only simple price field.
+ *
+ * @author Paweł Jędrzejewkski <pjedrzejewski@diweb.pl>
+ */
+class VariantType extends BaseVariantType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('price', 'number')
+        ;
+    }
+}
