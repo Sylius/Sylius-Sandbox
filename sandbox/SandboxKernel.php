deae4aabf4717e2665ec09822d09b4a511470c26
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-04-20 21:57:03 +0200

    update vendors, add some testing api for products.

--- sandbox/SandboxKernel.php
+++ sandbox/SandboxKernel.php
@@ -34,6 +34,8 @@ class SandboxKernel extends Kernel
             new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new \Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new \FOS\RestBundle\FOSRestBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle($this),
             new \Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new \Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
