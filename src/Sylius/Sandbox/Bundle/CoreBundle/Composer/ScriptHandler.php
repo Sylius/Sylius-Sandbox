e90882272f29ebc257dc25a3b40cb1a3cf5ec96d
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-05-02 18:13:32 +0200

    symlink twitter bootstrap instead of putting it directly in project.

---
+++ src/Sylius/Sandbox/Bundle/CoreBundle/Composer/ScriptHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sylius\Sandbox\Bundle\CoreBundle\Composer;
+
+use Composer\Script\Event;
+
+/**
+ * Symlinks twitter bootstrap.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class ScriptHandler
+{
+    /**
+     * Symlink twitter bootstrap.
+     *
+     * @param Event $event
+     */
+    static public function symlinkTwitterBootstrap(Event $event)
+    {
+        $IO = $event->getIO();
+        $composer = $event->getComposer();
+
+        $IO->write("<info>[Sylius]</info> Creating symlink for Twitter Bootstrap files.");
+
+        $symlinkTarget = __DIR__.'/../../../../../../vendor/twitter/bootstrap';
+        $symlinkName = __DIR__.'/../Resources/public/bootstrap';
+
+        if(false === symlink($symlinkTarget, $symlinkName)){
+            throw new \Exception('Error occured when trying to create a symlink.');
+        }
+    }
+}
