bb994155e33542455df952f20fce0f1268a32803
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-05-02 21:54:33 +0200

    fix composer script handler.

--- src/Sylius/Sandbox/Bundle/CoreBundle/Composer/ScriptHandler.php
+++ src/Sylius/Sandbox/Bundle/CoreBundle/Composer/ScriptHandler.php
@@ -21,12 +21,11 @@ class ScriptHandler
         $IO = $event->getIO();
         $composer = $event->getComposer();
 
-        $IO->write("<info>[Sylius]</info> Creating symlink for Twitter Bootstrap files.");
-
         $symlinkTarget = __DIR__.'/../../../../../../vendor/twitter/bootstrap';
         $symlinkName = __DIR__.'/../Resources/public/bootstrap';
 
-        if (!file_exists($symlinkTarget)) {
+        if (!@readlink($symlinkName)) {
+            $IO->write("<info>[Sylius]</info> Creating symlink for Twitter Bootstrap files.");
             if(false === symlink($symlinkTarget, $symlinkName)){
                 throw new \Exception('Error occured when trying to create a symlink.');
             }
