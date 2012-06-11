9caa35e1aeecd53c3c59d0e016bcae36cb816455
umpirsky <umpirsky@gmail.com>
2012-06-11 15:17:34 +0200

    Initial comment bundle implementation.

---
+++ src/Sylius/Sandbox/Bundle/CommentBundle/Entity/Comment.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+* This file is part of the Sylius sandbox application.
+*
+* (c) Paweł Jędrzejewski
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Sylius\Sandbox\Bundle\CommentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\CommentBundle\Entity\Comment as BaseComment;
+
+/**
+ * Comment entity.
+ *
+ * @author Саша Стаменковић <umpirsky@gmail.com>
+ */
+class Comment extends BaseComment
+{
+    /**
+     * Comment ID.
+     */
+    protected $id;
+
+    /**
+     * Thread of this comment.
+     *
+     * @var Thread
+     */
+    protected $thread;
+}
