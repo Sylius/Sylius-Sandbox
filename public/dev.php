68707f36c54a3c74705535f6d4eb0e778ca8981a
umpirsky <umpirsky@gmail.com>
2012-06-12 08:45:36 +0200

    Added role based ACL security.

--- public/dev.php
+++ public/dev.php
@@ -16,6 +16,8 @@ use Symfony\Component\HttpFoundation\Request;
  * Dev environment.
  */
 
+ini_set('xdebug.max_nesting_level', 200);
+
 // Require kernel.
 require_once __DIR__.'/../sandbox/SandboxKernel.php';
 
