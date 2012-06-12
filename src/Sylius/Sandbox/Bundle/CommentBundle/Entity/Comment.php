4f2f11548d2dc3b47832f9e4a3e55f14e9484320
umpirsky <umpirsky@gmail.com>
2012-06-12 08:30:37 +0200

    FOSUserBundle integration.

--- src/Sylius/Sandbox/Bundle/CommentBundle/Entity/Comment.php
+++ src/Sylius/Sandbox/Bundle/CommentBundle/Entity/Comment.php
@@ -13,13 +13,15 @@ namespace Sylius\Sandbox\Bundle\CommentBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Comment entity.
  *
  * @author Саша Стаменковић <umpirsky@gmail.com>
  */
-class Comment extends BaseComment
+class Comment extends BaseComment implements SignedCommentInterface
 {
     /**
      * Thread of this comment.
@@ -27,4 +29,45 @@ class Comment extends BaseComment
      * @var Thread
      */
     protected $thread;
+
+    /**
+     * Author of the comment
+     *
+     * @var UserInterface
+     */
+    protected $author;
+
+    /**
+     * Sets comment author.
+     *
+     * @param UserInterface $author
+     */
+    public function setAuthor(UserInterface $author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * Gets author.
+     *
+     * @return UserInterface
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Gets author name.
+     *
+     * @return string
+     */
+    public function getAuthorName()
+    {
+        if (null === $this->getAuthor()) {
+            return 'Anonymous';
+        }
+
+        return $this->getAuthor()->getUsername();
+    }
 }
